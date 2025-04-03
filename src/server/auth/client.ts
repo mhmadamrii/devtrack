311b1c4b5793c825c9b65d2c6d7a4c39ea8e9af9
mhmadamrii <interceptorghost4@gmail.com>
2025-04-03 18:39:20 +0700

    chore[env]: changed something instead

--- src/server/auth/client.ts
+++ src/server/auth/client.ts
@@ -1,7 +1,8 @@
 import { adminClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
+import { env } from '~/env';
 
 export const authClient = createAuthClient({
-  baseURL: 'http://localhost:3000',
+  baseURL: env.BETTER_AUTH_URL,
   plugins: [adminClient()],
 });
